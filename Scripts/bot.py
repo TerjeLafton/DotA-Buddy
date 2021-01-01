c0fb22d63dba50b722165e0aea5bc2040bd91c2b
terje@lafton.io <fnRFaqdywnsT4P9vfyngVUwJ>
2021-01-01 20:20:16 +0100

    Total rework...

--- Scripts/bot.py
+++ Scripts/bot.py
@@ -1,54 +1,75 @@
 import os
 import asyncio
-from contextvars import ContextVar, Context, copy_context
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
-# Assigns the Discord token to the TOKEN variable
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Creates the bot object with a prefix used for all commands given to the bot.
-bot = commands.Bot(command_prefix='!')
-
-# Create a Context Variable to tell when the clock should be running and set it to False
-clock_running = ContextVar('Clock Running')
-clock_running.set(False)
-# Create a Context Variable to store the message ID of the initial message created by the bot.
-# Used to match the on_reaction_add event to only the message created by the bot
-message_id = ContextVar('Message ID')
+client = commands.Bot(command_prefix='!')
 
 
-# Create a on_ready function to say when the bot is ready to go
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+class BotCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.message = None
+        self.clock_running = False
+        self.minutes = 0
+        self.seconds = 0
+        self.last_event = 'Match started!'
+
+        self.clock.start()
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'{self.bot.user.name} has successfully connected to the Discord server!')
+
+    @commands.command(name='time')
+    async def time(self, ctx):
+        self.message = await ctx.send(f'```Match has not started yet!\n\n'
+                                      f'Click the ✅ to start the match!```')
+        await self.message.add_reaction('✅')
+        await self.message.add_reaction('❎')
+        await self.message.add_reaction('⏮')
+        await self.message.add_reaction('⏪')
+        await self.message.add_reaction('⏩')
+        await self.message.add_reaction('⏭')
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if str(user).split('#')[0] != self.bot.user.name:
+            if reaction.message.id == self.message.id:
+                if reaction.emoji == '✅':
+                    self.clock_running = True
+                elif reaction.emoji == '❎':
+                    self.clock_running = False
+                elif reaction.emoji == '⏮':
+                    self.seconds -= 10
+                elif reaction.emoji == '⏪':
+                    self.seconds -= 1
+                elif reaction.emoji == '⏩':
+                    self.seconds += 1
+                elif reaction.emoji == '⏭':
+                    self.seconds += 10
+
+    @tasks.loop(seconds=1)
+    async def clock(self):
+        if self.message:
+            if self.clock_running:
+                if self.seconds == 5:
+                    self.last_event = f'{self.minutes}:{self.seconds}: Bounty rune in 15 seconds!'
+
+                if self.seconds == 59:
+                    self.seconds = 0
+                    self.minutes += 1
+                else:
+                    self.seconds += 1
+
+            await self.message.edit(content=f'```Match has started!\n\n'
+                                            f'Time: {self.minutes}:{self.seconds}\n\n'
+                                            f'Last event -  {self.last_event}```')
 
 
-# The main bot command to initiate the bots functionality
-@bot.command(name='time')
-async def time(ctx):
-    # Send the initial message, store it in a variable and create reactions
-    message = await ctx.send(f'```Match not started yet :( \n\n'
-                             f'Click the ✅ to start the match!```')
-    await message.add_reaction('✅')
-    await message.add_reaction('❎')
-
-    # Store the message ID in the Context Variable created earlier
-    message_id.set(message.id)
-    print(message_id.get())
-
-
-# Function to define what happens if anyone reacts to the initial bot message
-# We use this to control the clock and such
-@bot.event
-async def on_reaction_add(reaction, user):
-    # Make sure to only look for reactions not made by the bot
-    if str(user).split('#')[0] != bot.user.name:
-        print(reaction.me)
-        print(message_id.get())
-
-
-bot.run(TOKEN)
+client.add_cog(BotCog(client))
+client.run(TOKEN)
